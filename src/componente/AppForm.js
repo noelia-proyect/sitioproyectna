53f88df97709e31ae94950d64ef8a751e4215927
Noelia <nccohuanquichuctaya@gmail.com>
2022-11-10 19:44:05 -0500

    Version-examen

--- src/componente/AppForm.js
+++ src/componente/AppForm.js
@@ -5,7 +5,7 @@ import firebase, { db } from './firebase' ;
 import Select from 'react-select'
 const AppForm = (props) => {
     ////////////////////actualizar y eliminar///////////////////////
-  const camposRegistro = {nombre:'',edad:"",genero:""}; ////estructura de campo de resgitro/////
+  const camposRegistro = {url:'',nombre:"",descripcion:""}; ////estructura de campo de resgitro/////
   const [objeto,setObjeto] = useState(camposRegistro);
    
   const controlarEstadoCambio= async(e) => {///////////////maneja cambios en input//////////////////
@@ -18,7 +18,7 @@ const AppForm = (props) => {
      e.preventDefault(); ////evita por defecto
      if (props.idActual === ""){
         if(validarForm()){
-            addDoc(collection(db, 'persona'),objeto);
+            addDoc(collection(db, 'favoritos'),objeto);
             console.log("Se guardo en bd");
 
         }else{
@@ -30,16 +30,16 @@ const AppForm = (props) => {
     setObjeto(camposRegistro);
   };
   const validarForm= (e) => {
-    if(objeto.nombre==="" || /^\s+$/.test(objeto.nombre)){
-       alert("Escriba nombre...");
+    if(objeto.url==="" || /^\s+$/.test(objeto.url)){
+       alert("URL");
        return false;
       }
-    if (objeto.edad ==="" ||/^\s+$/.test(objeto.edad)){
-       alert("Escriba su edad...")
+    if (objeto.nombre ==="" ||/^\s+$/.test(objeto.nombre)){
+       alert("Nombre")
        return false;
     }
-    if (objeto.genero ==="" ||/^\s+$/.test(objeto.genero)){
-      alert("Escriba su edad...")
+    if (objeto.descripcion ==="" ||/^\s+$/.test(objeto.descripcion)){
+      alert("Descripcion")
       return false;
    }
     return true;
@@ -50,13 +50,9 @@ const AppForm = (props) => {
     <div style={{background:"#123748",height:"489px",width:"350px",boxShadow: "7px 13px 37px #000", paddingTop:"7px"}}>
     <h3 style={{fontSize: "30px", marginBottom: "0px"}}>Registro</h3>
     <form onSubmit={controlSubmit} style={{ padding:"40px", fontFamily: "calibri"}}>
-      <input style={{width:"100%", background:"#123748", padding:"10px", borderRadius:"4px", marginBottom:"20px", border: "2px solid #1f53c5", fontFamily: "calibri", fontSize:"18px", color:"white"}} type="text" name='nombre' placeholder='Ingrese su nombre' onChange={controlarEstadoCambio} value={objeto.nombre}/> <br />
-      <input style={{width:"100%", background:"#123748", padding:"10px", borderRadius:"4px", marginBottom:"20px", border: "2px solid #1f53c5", fontFamily: "calibri", fontSize:"18px", color:"white"}} type="text" name='edad' placeholder='Ingrese su edad' onChange={controlarEstadoCambio} value={objeto.edad}/> <br />
-      <select style={{width:"108%", background:"#123748", padding:"10px", borderRadius:"4px", marginBottom:"20px", border: "2px solid #1f53c5", fontFamily: "calibri", fontSize:"18px", color:"white"}} type="text" name="genero" onChange={controlarEstadoCambio}value={objeto.genero} >
-        <option >Seleccione su genero</option>
-        <option value={"femenino"}>Femenino</option>
-        <option value={"masculino"}>Masculino</option>
-      </select>
+      <input style={{width:"100%", background:"#123748", padding:"10px", borderRadius:"4px", marginBottom:"20px", border: "2px solid #1f53c5", fontFamily: "calibri", fontSize:"18px", color:"white"}} type="text" name='url' placeholder='URL' onChange={controlarEstadoCambio} value={objeto.url}/> <br />
+      <input style={{width:"100%", background:"#123748", padding:"10px", borderRadius:"4px", marginBottom:"20px", border: "2px solid #1f53c5", fontFamily: "calibri", fontSize:"18px", color:"white"}} type="text" name='nombre' placeholder='Nombre' onChange={controlarEstadoCambio} value={objeto.nombre}/> <br />
+      <input style={{width:"100%", background:"#123748", padding:"10px", borderRadius:"4px", marginBottom:"20px", border: "2px solid #1f53c5", fontFamily: "calibri", fontSize:"18px", color:"white"}} type="text" name='descripcion' placeholder='Descripcion' onChange={controlarEstadoCambio} value={objeto.descripcion}/> <br />
       <button style={{width:"100%", background:"#1f53c5", border: "none",padding:"12px", color:"white", margin:"16px 15px", fontSize: "18px", fontFamily: "calibri"}}>
       {props.idActual === ""? "Guardar" : "Actualizar"}
       </button>
